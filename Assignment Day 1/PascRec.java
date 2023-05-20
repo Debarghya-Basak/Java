e4e8f144b1735d49ff3a7cbf3f68d3ee40783cf3
Debarghya Basak <106986982+Debarghya-Basak@users.noreply.github.com>
2023-05-20 13:31:35 +0530

    Update PascRec.java

--- Assignment Day 1/PascRec.java
+++ Assignment Day 1/PascRec.java
@@ -1,1 +1,46 @@
 
+public class PascRec {
+
+  int pascCalc(int r, int c){
+
+    if(c == 0 || c == 1)
+
+     return c;
+
+    else if(c == r+1 || r == 0)
+
+     return 0;
+
+     
+
+     
+
+    return pascCalc(r-1, c-1)+pascCalc(r-1, c);
+
+  }
+
+  public static void main(String[] args) {
+
+     PascRec ob = new PascRec();
+
+     int n = 5;
+
+     for(int i=1;i<=n;i++){
+
+       for(int j=1;j<=n-i;j++)
+
+         System.out.print(" ");
+
+     
+
+       for(int j=1;j<=i;j++)
+
+      System.out.print(ob.pascCalc(i,j)+ " ");
+
+      System.out.println();
+
+      }
+
+  }
+
+}
